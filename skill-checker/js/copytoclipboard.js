69e3efca521e685b7554a067e7dd315f62611fd9
Kaspar-PotBS <94735202+Kaspar-PotBS@users.noreply.github.com>
2021-12-07 21:09:49 +0100

    Add files via upload

--- skill-checker/js/copytoclipboard.js
+++ skill-checker/js/copytoclipboard.js
@@ -8,21 +8,3 @@ function CopyCodeToClipboard() {
   tooltip.innerHTML = "Copied: " + copyText.value;
 }
 
-function outFunc() {
-  var tooltip = document.getElementById("URLField");
-  tooltip.innerHTML = "Copy to clipboard";
-};
-function CopyURLToClipboard() {
-  var copyText = document.getElementById("URLField");
-  copyText.select();
-  copyText.setSelectionRange(0, 99999);
-  navigator.clipboard.writeText(copyText.value);
-  
-  var tooltip = document.getElementById("copybtn-tooltip2");
-  tooltip.innerHTML = "Copied: " + copyText.value;
-}
-
-function outFunc() {
-  var tooltip = document.getElementById("URLField");
-  tooltip.innerHTML = "Copy to clipboard";
-};
